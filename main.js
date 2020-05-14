3b42bdc3fc05e170956eb7a66f2956c8d9c787b1
Kristjan Fale <kristjan.fale@gmail.com>
2020-05-14 16:24:09 +0200

    JS Hamburger menu

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const nav = document.querySelector('.nav');
 
+// Toggle navigation
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
+});
+
+// Close navigation when click on one of the links
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
+});
+
+// Close navigation when click outside of navigation list
+nav.addEventListener('click', () => {
+  document.body.classList.remove('nav-open');
 });

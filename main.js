9c62a7343750e34fb0113296c7f1938d0373f4d4
Kristjan Fale <kristjan.fale@gmail.com>
2020-06-03 20:31:44 +0200

    Section portfolio

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 const nav = document.querySelector('.nav');
 
+const cardText = document.querySelectorAll('.card__text');
+
+// NAVIGATION
 // Toggle navigation
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
@@ -18,3 +21,30 @@ navLinks.forEach((link) => {
 nav.addEventListener('click', () => {
   document.body.classList.remove('nav-open');
 });
+// END NAVIGATION
+
+// CARD BUTTONS
+cardText.forEach((card) => {
+  // Hover over first(button--live)
+  card.children[0].addEventListener('mouseover', () => {
+    card.children[0].style.transform = 'scale(1.1)';
+    card.children[1].style.transform = 'scale(0.9)';
+  });
+
+  card.children[0].addEventListener('mouseout', () => {
+    card.children[0].style.transform = 'scale(1)';
+    card.children[1].style.transform = 'scale(1)';
+  });
+
+  // Hover over second(button--code)
+  card.children[1].addEventListener('mouseover', () => {
+    card.children[0].style.transform = 'scale(0.9)';
+    card.children[1].style.transform = 'scale(1.1)';
+  });
+
+  card.children[1].addEventListener('mouseout', () => {
+    card.children[0].style.transform = 'scale(1)';
+    card.children[1].style.transform = 'scale(1)';
+  });
+});
+// END CARD BUTTONS
